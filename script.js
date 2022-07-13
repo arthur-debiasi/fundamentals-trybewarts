ca0f771b1e07ad44e34ac53896eb0610e9c782c6
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-07-13 19:09:49 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const password = document.getElementById('password');
 const loginBtn = document.getElementById('login-btn');
 const submitBtn = document.getElementById('submit-btn');
 const agreement = document.getElementById('agreement');
+const comment = document.getElementById('textarea');
+const commentCounter = document.getElementById('counter');
 
 function loginValidation() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -20,3 +22,9 @@ function submitValidation() {
 }
 
 agreement.addEventListener('change', submitValidation);
+
+function characterCounter() {
+  commentCounter.innerText = 500 - (comment.value.length);
+}
+
+comment.addEventListener('input', characterCounter);
